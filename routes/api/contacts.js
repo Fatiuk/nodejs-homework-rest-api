0e2edfeab0d1251b0ccb104655a09114323b36cb
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-11-26 17:16:33 +0200

    feat: added joi schemas and update error handling

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,36 +1,41 @@
 const express = require("express");
+const router = express.Router();
 
 const contacts = require("../../models/contacts");
-
-const router = express.Router();
+const { HttpError } = require("../../helpers");
+const { addSchema } = require("../../schemas/contacts");
 
 router.get("/", async (req, res, next) => {
   try {
     const result = await contacts.listContacts();
     res.status(200).json(result);
   } catch (error) {
-    res.status(500).json({ message: "Server error!" });
     next(error);
   }
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
   try {
+    const { contactId } = req.params;
     const result = await contacts.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
     res.status(200).json(result);
   } catch (error) {
-    res.status(500).json({ message: "Server error!" });
     next(error);
   }
 });
 
 router.post("/", async (req, res, next) => {
   try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
     const result = await contacts.addContact(req.body);
     res.status(201).json(result);
   } catch (error) {
-    res.status(500).json({ message: "Server error!" });
     next(error);
   }
 });
@@ -39,9 +44,11 @@ router.delete("/:contactId", async (req, res, next) => {
   const { contactId } = req.params;
   try {
     const result = await contacts.removeContact(contactId);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
     res.json({ message: "Delete success" });
   } catch (error) {
-    res.status(500).json({ message: "Server error!" });
     next(error);
   }
 });
