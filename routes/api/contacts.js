f331f29f59ef04c4de6091901d19a45bea0678dc
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-11-26 16:11:47 +0200

    feat (Express): added routes functional

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,21 +1,49 @@
 const express = require("express");
 
+const contacts = require("../../models/contacts");
+
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const result = await contacts.listContacts();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: "Server error!" });
+    next(error);
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { contactId } = req.params;
+  try {
+    const result = await contacts.getContactById(contactId);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: "Server error!" });
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ message: "Server error!" });
+    next(error);
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const { contactId } = req.params;
+  try {
+    const result = await contacts.removeContact(contactId);
+    res.json({ message: "Delete success" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error!" });
+    next(error);
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
