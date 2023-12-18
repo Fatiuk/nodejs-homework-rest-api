5a843d50ebeab3786cab735239c69d1a1f2654f0
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-12-18 14:02:24 +0200

    feat: configuration db with .env

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const { DB_SERVER_HOST, DB_SERVER_PORT = 8080 } = process.env;
+
+mongoose
+  .connect(DB_SERVER_HOST)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(DB_SERVER_PORT);
+    console.log(`Server is running on port ${DB_SERVER_PORT}`);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
