6a349c8cdd00709cebc18b8f42e407a205ed8eee
Fatiuk <125760945+Fatiuk@users.noreply.github.com>
2023-12-25 22:14:02 +0200

    feat: added pagination

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,10 +3,12 @@ const { httpError, ctrlWrapper } = require("../helpers");
 
 const getContactsList = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find(
-    { owner },
-    "-createdAt -updatedAt"
-  ).populate("owner", "name email");
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "name email");
   res.status(200).json(result);
 };
 
